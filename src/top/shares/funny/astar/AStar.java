5ceec506718e113fda22c46bb2eee97fb24014dd
MonsterSlayer <slaying.monsters@gmail.com>
2015-07-23 09:36:44 +0800

    add some file wait to be implemented

--- src/top/shares/funny/astar/AStar.java
+++ src/top/shares/funny/astar/AStar.java
@@ -2,9 +2,35 @@ package top.shares.funny.astar;
 /*
  *  A Star search algorithm
  * 
+ * given a two-dimensional matix, find the shortest way to the destination
+ * 
+ * how to find a shortest way from A to D, and 1 stands cannot pass
+ * 
+ * [ 
+ * 0 0 0 0 0 0 0 0 0 0 0 0 
+ * 0 0 0 0 0 0 0 0 1 0 D 0 
+ * 0 0 0 0 1 0 0 0 1 0 0 0 
+ * 0 0 A 0 1 0 0 0 1 0 0 0 
+ * 0 0 0 0 1 0 0 0 0 0 0 0 
+ * 0 0 0 0 0 0 0 0 0 0 0 0 
+ * 0 0 0 0 0 0 0 0 0 0 0 0 
+ * ]
+ * 
+ * 
  * @author dongyado<dongaydo@gmail.com>
  * 
  * */
 public class AStar {
+	
+	public class Point {
+		public int value;
+	}
+	
+	
+	public static Boolean move()
+	{
+		return false;
+	}
+	
 
 }
---
+++ src/top/shares/funny/lru/HashMap.java
@@ -0,0 +1,16 @@
+package top.shares.funny.lru;
+
+/**
+ * HashMap implements
+ * 
+ * 
+ * @author dongyado<dongyado@gmail.com>
+ * 
+ * */
+
+public class HashMap {
+	
+	
+	
+
+}
